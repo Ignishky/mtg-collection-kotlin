8b711c81625d6f7c5f4fa0c702d0cb010a16fed0
ignishky <thierry.ducloyer@gmail.com>
2022-11-24 20:50:06 +0100

    chore: initial commit

---
+++ src/main/kotlin/fr/ignishky/mtgcollection/MtgCollectionKotlinApplication.kt
@@ -0,0 +1,11 @@
+package fr.ignishky.mtgcollection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MtgCollectionKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<MtgCollectionKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/fr/ignishky/mtgcollection/MtgCollectionKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.ignishky.mtgcollection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MtgCollectionKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
