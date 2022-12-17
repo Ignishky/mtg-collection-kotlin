7ee57c53905fd078588c614aa3d927da0012c52f
ignishky <thierry.ducloyer@gmail.com>
2022-12-17 15:47:20 +0100

    build(production): add configuration file

--- src/main/kotlin/fr/ignishky/mtgcollection/configuration/ApplicationConfiguration.kt
+++ src/main/kotlin/fr/ignishky/mtgcollection/configuration/ApplicationConfiguration.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile
 import java.time.Clock
 
 @Configuration
-@Profile("production")
+@Profile("!test")
 class ApplicationConfiguration {
 
     @Bean
