80302bce7c8862921f3257cb2f512baf20bc564c
ignishky <thierry.ducloyer@gmail.com>
2023-02-22 19:28:27 +0100

    feat(api): add endpoint to retrieve cards by set
    Contains only the card's name.

--- src/main/kotlin/fr/ignishky/mtgcollection/infrastructure/api/rest/GlobalController.kt
+++ src/main/kotlin/fr/ignishky/mtgcollection/infrastructure/api/rest/GlobalController.kt
@@ -3,8 +3,11 @@ package fr.ignishky.mtgcollection.infrastructure.api.rest
 import fr.ignishky.framework.cqrs.command.CommandBus
 import fr.ignishky.framework.domain.CorrelationId
 import fr.ignishky.mtgcollection.domain.card.command.RefreshCard
+import fr.ignishky.mtgcollection.domain.card.port.CardApiPort
 import fr.ignishky.mtgcollection.domain.set.command.RefreshSet
+import fr.ignishky.mtgcollection.domain.set.model.SetCode
 import fr.ignishky.mtgcollection.domain.set.port.SetApiPort
+import fr.ignishky.mtgcollection.infrastructure.api.rest.CardsResponse.CardResponse
 import fr.ignishky.mtgcollection.infrastructure.api.rest.SetsResponse.SetResponse
 import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.http.ResponseEntity
@@ -13,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 internal class GlobalController(
     private val commandBus: CommandBus,
-    private val setApiPort: SetApiPort
+    private val setApiPort: SetApiPort,
+    private val cardApiPort: CardApiPort
 ) : GlobalApi {
 
     override fun loadAll(correlationId: CorrelationId): ResponseEntity<Void> {
@@ -26,6 +30,13 @@ internal class GlobalController(
         val sets = setApiPort.getAll()
         return SetsResponse(
             sets.map { SetResponse(it.code.value, it.name.value, it.icon.value) }
+        )
+    }
+
+    override fun getCards(correlationId: CorrelationId, setCode: String): CardsResponse {
+        val cards = cardApiPort.getAll(SetCode(setCode))
+        return CardsResponse(
+            cards.map { CardResponse(it.name.value) }
         )
     }
 
