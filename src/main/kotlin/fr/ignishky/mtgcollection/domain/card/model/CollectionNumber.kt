8f3434035b89e7b7d40a4e5cdf0ebbb74fa74005
ignishky <thierry.ducloyer@gmail.com>
2023-03-12 21:41:46 +0100

    fix(card): correctly compare collection number

--- src/main/kotlin/fr/ignishky/mtgcollection/domain/card/model/CollectionNumber.kt
+++ src/main/kotlin/fr/ignishky/mtgcollection/domain/card/model/CollectionNumber.kt
@@ -5,6 +5,7 @@ data class CollectionNumber(internal val value: String) : Comparable<CollectionN
     override fun compareTo(other: CollectionNumber): Int {
         val a = value.toIntOrNull()
         val b = other.value.toIntOrNull()
+        if (a == b) return 0
         if (a == null) return 1
         if (b == null) return -1
 
